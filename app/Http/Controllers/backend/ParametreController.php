554b28de6ff4a0a71504ae15fd29d47ef9ed8d3d
davyalex <alexkouamelan96pv@gmail.com>
2025-06-17 13:01:21 +0000

    optimize

--- app/Http/Controllers/backend/ParametreController.php
+++ app/Http/Controllers/backend/ParametreController.php
@@ -50,120 +50,180 @@ class ParametreController extends Controller
     }
 
 
+    // public function store(Request $request)
+    // {
+    //     try {
+    //         //request validation................
+    //         // dd($request->all());
+
+    //         //verify if data exist
+    //         $data_exist = Parametre::with('media')->first();
+
+
+    //         if ($data_exist) {
+    //             // dd($request->all());
+
+    //             $data_exist_ = Parametre::with('media')->first();
+    //             $media = $data_exist_->media;
+    //             // dd(count($data_exist_->media));
+
+
+    //             //insert data
+    //             //update data if record exist
+    //             $data_parametre = tap(Parametre::find($data_exist_['id']))->update([
+    //                 'lien_facebook' => $request['lien_facebook'],
+    //                 'lien_instagram' => $request['lien_instagram'],
+    //                 'lien_twitter' => $request['lien_twitter'],
+    //                 'lien_linkedin' => $request['lien_linkedin'],
+    //                 'lien_tiktok' => $request['lien_tiktok'],
+    //                 'lien_youtube' => $request['lien_youtube'],
+
+    //                 //infos application
+    //                 'nom_projet' => $request['nom_projet'],
+    //                 'description_projet' => $request['description_projet'],
+    //                 'contact1' => $request['contact1'],
+    //                 'contact2' => $request['contact2'],
+    //                 'contact3' => $request['contact3'],
+
+    //                 'email1' => $request['email1'],
+    //                 'email2' => $request['email2'],
+
+    //                 'localisation' => $request['localisation'],
+    //                 'google_maps' => $request['google_maps'],
+    //                 'siege_social' => $request['siege_social'],
+
+    //                 //security
+    //                 // 'mode_maintenance'=>'',
+    //             ]);
+
+    //             //insert image logo
+
+    //             if ($request->has('cover') && count($media) > 0) {
+    //                 $data_parametre->clearMediaCollection('cover');
+    //                 $data_parametre->addMediaFromRequest('cover')->toMediaCollection('cover');
+    //             } elseif ($request->has('cover')) {
+    //                 $data_parametre->addMediaFromRequest('cover')->toMediaCollection('cover');
+    //             }
+
+    //             if ($request->has('logo_header') && count($media) > 0) {
+    //                 $data_parametre->clearMediaCollection('logo_header');
+    //                 $data_parametre->addMediaFromRequest('logo_header')->toMediaCollection('logo_header');
+    //             } elseif ($request->has('logo_header')) {
+    //                 $data_parametre->addMediaFromRequest('logo_header')->toMediaCollection('logo_header');
+    //             }
+
+
+    //             if ($request->has('logo_footer') && count($media) > 0) {
+    //                 $data_parametre->clearMediaCollection('logo_footer');
+    //                 $data_parametre->addMediaFromRequest('logo_footer')->toMediaCollection('logo_footer');
+    //             } elseif ($request->has('logo_footer')) {
+    //                 $data_parametre->addMediaFromRequest('logo_footer')->toMediaCollection('logo_footer');
+    //             }
+    //         } else {
+    //             $data_parametre = Parametre::create([
+    //                 'lien_facebook' => $request['lien_facebook'],
+    //                 'lien_instagram' => $request['lien_instagram'],
+    //                 'lien_twitter' => $request['lien_twitter'],
+    //                 'lien_linkedin' => $request['lien_linkedin'],
+    //                 'lien_tiktok' => $request['lien_tiktok'],
+    //                 'lien_youtube' => $request['lien_youtube'],
+
+    //                 //infos application
+    //                 'nom_projet' => $request['nom_projet'],
+    //                 'description_projet' => $request['description_projet'],
+    //                 'contact1' => $request['contact1'],
+    //                 'contact2' => $request['contact2'],
+    //                 'contact3' => $request['contact3'],
+
+    //                 'email1' => $request['email1'],
+    //                 'email2' => $request['email2'],
+
+    //                 'localisation' => $request['localisation'],
+    //                 'google_maps' => $request['google_maps'],
+    //                 'siege_social' => $request['siege_social'],
+
+    //                 //security
+    //                 // 'mode_maintenance'=>'',
+    //             ]);
+
+    //             //insert image logo
+    //             if ($request->has('logo_header')) {
+    //                 $data_parametre->addMediaFromRequest('logo_header')->toMediaCollection('logo_header');
+    //             }
+
+
+    //             if ($request->has('logo_footer')) {
+    //                 $data_parametre->addMediaFromRequest('logo_footer')->toMediaCollection('logo_footer');
+    //             }
+    //         }
+
+
+
+    //         Alert::success('Operation réussi', 'Success Message');
+
+    //         return back();
+    //     } catch (\Throwable $th) {
+    //         return back()->withError($th->getMessage());
+    //     }
+    // }
+
+
+    /**VERSION OPTIMISE */
+
     public function store(Request $request)
     {
         try {
-            //request validation................
-            // dd($request->all());
+            // Vérifie s'il existe déjà un paramètre
+            $parametre = Parametre::with('media')->first();
 
-            //verify if data exist
-            $data_exist = Parametre::with('media')->first();
+            // Données à créer ou mettre à jour
+            $data = $request->only([
+                'lien_facebook',
+                'lien_instagram',
+                'lien_twitter',
+                'lien_linkedin',
+                'lien_tiktok',
+                'lien_youtube',
+                'nom_projet',
+                'description_projet',
+                'contact1',
+                'contact2',
+                'contact3',
+                'email1',
+                'email2',
+                'localisation',
+                'google_maps',
+                'siege_social'
+            ]);
 
-
-            if ($data_exist) {
-                // dd($request->all());
-
-                $data_exist_ = Parametre::with('media')->first();
-                $media = $data_exist_->media;
-                // dd(count($data_exist_->media));
-
-
-                //insert data
-                //update data if record exist
-                $data_parametre = tap(Parametre::find($data_exist_['id']))->update([
-                    'lien_facebook' => $request['lien_facebook'],
-                    'lien_instagram' => $request['lien_instagram'],
-                    'lien_twitter' => $request['lien_twitter'],
-                    'lien_linkedin' => $request['lien_linkedin'],
-                    'lien_tiktok' => $request['lien_tiktok'],
-
-                    //infos application
-                    'nom_projet' => $request['nom_projet'],
-                    'description_projet' => $request['description_projet'],
-                    'contact1' => $request['contact1'],
-                    'contact2' => $request['contact2'],
-                    'contact3' => $request['contact3'],
-
-                    'email1' => $request['email1'],
-                    'email2' => $request['email2'],
-
-                    'localisation' => $request['localisation'],
-                    'google_maps' => $request['google_maps'],
-                    'siege_social' => $request['siege_social'],
-
-                    //security
-                    // 'mode_maintenance'=>'',
-                ]);
-
-                //insert image logo
-
-                if ($request->has('cover') && count($media) > 0) {
-                    $data_parametre->clearMediaCollection('cover');
-                    $data_parametre->addMediaFromRequest('cover')->toMediaCollection('cover');
-                } elseif ($request->has('cover')) {
-                    $data_parametre->addMediaFromRequest('cover')->toMediaCollection('cover');
-                }
-
-                if ($request->has('logo_header') && count($media) > 0) {
-                    $data_parametre->clearMediaCollection('logo_header');
-                    $data_parametre->addMediaFromRequest('logo_header')->toMediaCollection('logo_header');
-                } elseif ($request->has('logo_header')) {
-                    $data_parametre->addMediaFromRequest('logo_header')->toMediaCollection('logo_header');
-                }
-
-
-                if ($request->has('logo_footer') && count($media) > 0) {
-                    $data_parametre->clearMediaCollection('logo_footer');
-                    $data_parametre->addMediaFromRequest('logo_footer')->toMediaCollection('logo_footer');
-                } elseif ($request->has('logo_footer')) {
-                    $data_parametre->addMediaFromRequest('logo_footer')->toMediaCollection('logo_footer');
-                }
+            // Création ou mise à jour
+            if ($parametre) {
+                $parametre->update($data);
             } else {
-                $data_parametre = Parametre::create([
-                    'lien_facebook' => $request['lien_facebook'],
-                    'lien_instagram' => $request['lien_instagram'],
-                    'lien_twitter' => $request['lien_twitter'],
-                    'lien_linkedin' => $request['lien_linkedin'],
-                    'lien_tiktok' => $request['lien_tiktok'],
+                $parametre = Parametre::create($data);
+            }
 
-                    //infos application
-                    'nom_projet' => $request['nom_projet'],
-                    'description_projet' => $request['description_projet'],
-                    'contact1' => $request['contact1'],
-                    'contact2' => $request['contact2'],
-                    'contact3' => $request['contact3'],
+            // Gestion des fichiers à uploader
+            $mediaFields = ['cover', 'logo_header', 'logo_footer'];
+            foreach ($mediaFields as $field) {
+                if ($request->hasFile($field)) {
+                    // Supprime l'ancien média s’il existe
+                    if ($parametre->hasMedia($field)) {
+                        $parametre->clearMediaCollection($field);
+                    }
 
-                    'email1' => $request['email1'],
-                    'email2' => $request['email2'],
-
-                    'localisation' => $request['localisation'],
-                    'google_maps' => $request['google_maps'],
-                    'siege_social' => $request['siege_social'],
-
-                    //security
-                    // 'mode_maintenance'=>'',
-                ]);
-
-                //insert image logo
-                if ($request->has('logo_header')) {
-                    $data_parametre->addMediaFromRequest('logo_header')->toMediaCollection('logo_header');
-                }
-
-
-                if ($request->has('logo_footer')) {
-                    $data_parametre->addMediaFromRequest('logo_footer')->toMediaCollection('logo_footer');
+                    // Ajoute le nouveau fichier
+                    $parametre->addMediaFromRequest($field)->toMediaCollection($field);
                 }
             }
 
-
-
-            Alert::success('Operation réussi', 'Success Message');
-
+            Alert::success('Opération réussie', 'Les paramètres ont été enregistrés avec succès.');
             return back();
         } catch (\Throwable $th) {
             return back()->withError($th->getMessage());
         }
     }
+
 
 
     /**
